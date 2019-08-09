cf41038bf681e1d29808ae825e78f725083c141d
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-08-09 11:01:29 -0700

    completed MVP

--- BE/form-management/src/App.test.js
+++ BE/form-management/src/App.test.js
@@ -4,25 +4,15 @@ import "@testing-library/react/cleanup-after-each";
 import App from "./App.js";
 
 describe("<App />", () => {
+  // Test one event
   it("renders without crashing", () => {
     render(<App />);
   });
-  it("One Big Test", () => {
-    const { getByText } = render(<App />);
-    const strike = getByText(/^strike$/i);
-    const ball = getByText(/^ball$/i);
-    const foul = getByText(/^foul$/i);
-    const hit = getByText(/^hit$/i);
-
-    // Test that strike works
-    fireEvent.click(strike);
-    fireEvent.click(strike);
-    getByText(/Strikes: 2/);
-
-    // Test that ball works
-    fireEvent.click(ball);
-    fireEvent.click(ball);
-    fireEvent.click(ball);
-    getByText(/Balls: 3/);
+  it("submit event works", () => {
+    const submitButton = getByText("submit");
+    fireEvent.click(submitButton);
+  });
+  it("data appears on page", () => {
+    const dataItem = getByText("Hush Puppies");
   });
 });
